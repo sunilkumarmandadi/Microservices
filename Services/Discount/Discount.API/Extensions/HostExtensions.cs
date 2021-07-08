d8df04b7bf9596bf170bd14e81ea669c8666a6cc
sunilkumarmandati <sunilkumar.mandati@gmail.com>
2021-07-08 07:24:50 -0700

    discount api

---
+++ Services/Discount/Discount.API/Extensions/HostExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Extensions
+{
+    public class HostExtensions
+    {
+
+    }
+}
---
+++ Services/Discount/Discount.API/Repositories/DiscountRepository.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public class DiscountRepository : IDiscountRepository
+    {
+        private readonly IConfiguration _configuration;
+
+        public DiscountRepository(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+    }
+}
